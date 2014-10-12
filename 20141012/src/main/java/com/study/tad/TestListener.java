3082346aa0b454c448bfad00600da7287ba4281d
Tad <Tad@223.194.132.159>
2014-10-12 12:32:11 +0900

    first_test

---
+++ 20141012/src/main/java/com/study/tad/TestListener.java
@@ -0,0 +1,19 @@
+package com.study.tad;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class TestListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("잘되냥");
+	}
+
+}
